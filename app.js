e69be4c7405c807497a47c1c1b02dce1b2ee11b5
ssahibsingh <snone181@gmail.com>
2023-06-17 20:02:44 +0530

    updated

--- app.js
+++ app.js
@@ -2,28 +2,18 @@
 let count = 0;
 // select value and buttons
 const value = document.querySelector("#value");
-const btns = document.querySelectorAll(".btn");
 
-btns.forEach(function (btn) {
-  btn.addEventListener("click", function (e) {
-    const styles = e.currentTarget.classList;
-    if (styles.contains("decrease")) {
-      count--;
-    } else if (styles.contains("increase")) {
-      count++;
-    } else {
-      count = 0;
-    }
-
-    if (count > 0) {
-      value.style.color = "green";
-    }
-    if (count < 0) {
-      value.style.color = "red";
-    }
-    if (count === 0) {
-      value.style.color = "#222";
-    }
+const reset = () =>{
+    count = 0;
     value.textContent = count;
-  });
-});
+}
+
+const decrease = () =>{
+    count--;
+    value.textContent = count;
+}
+
+const increase = () =>{
+    count++;
+    value.textContent = count;
+}
